7f8832caa6be9ebc1abd334c1a13afcfbc790807
justinavickers <justinav38@gmail.com>
2019-02-11 09:53:03 -0600

    added reverse,to reverse all jobs

--- Lightning-2-11-19/fetchingData.js
+++ Lightning-2-11-19/fetchingData.js
@@ -2,7 +2,7 @@ fetch("https://api.stevebrownlee.com/jobs/")
     .then(jobs => jobs.json())
     .then(parsedJobs => {
         console.table(parsedJobs)
-       const allJobs = parsedJobs.results
+       const allJobs = parsedJobs.results.reverse()
        allJobs.forEach(job => {
           const jobAsHTML = jobFactory(job)
           addJobsToDom(jobAsHTML)
